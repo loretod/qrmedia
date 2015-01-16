e82efacec0a509db4c3928d7f199d256893ab33b
loretod <loretod101@gmail.com>
2015-01-15 22:53:51 -0500

    sign up add

--- web/js/qrmedia.js
+++ web/js/qrmedia.js
@@ -18,12 +18,12 @@ function saveMedia(type)
 
 }
 
-function confirmMedia()
+function previewMedia()
 {
 	//create a link/qrcode to it
 	//upload that to cloud
+	showPage()
 }
-
 
 
 $( document ).ready(function() {
@@ -31,8 +31,18 @@ $( document ).ready(function() {
 	$("#audio_btn").click(function(e){ showPage("#record_audio"); });
 	
 
-	$("#save_video").click( function(e){ saveMedia("video"); } );
-	$("#save_audio").click( function(e){ saveMedia("audio"); } );
+	$("#save_video").click( function(e){ showPage("#preview"); });
+		//event.saveMedia("video") ; 
+		
+	$("#save_audio").click( function(event){ 
+		event.saveMedia("audio") ;
+		event.showPage("#audio_preview")
+	} );
+
+	$("#create account").click( function(e){ showPage("#create account"); });
+
+	//$("#preview_video").click( function(e){ showPage("video_preview"); } );showPage("audio_preview")
+	//$("#preview").click( function(e){ ; } );
 
 
 });
